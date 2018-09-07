366289cc9ac8a4132f559f51258cbd231baacb6d
Adam Cotner <acotner@tendrilinc.com>
2018-09-07 09:27:45 -0600

    add put

--- src/register.js
+++ src/register.js
@@ -2,10 +2,13 @@ module.exports = (app, registry) => {
 	 for (let { route, method, responder } of registry) {
 		switch(method) {
 			case 'GET':
-				app.get(route, (req, res) => res.send({ method: 'GET' }));
+				app.get(route, createResponse(responder));
 				break;
 			case 'POST':
 				app.post(route, createResponse(responder));
+				break;
+			case 'PUT':
+				app.put(route, createResponse(responder));
 				break;
 			default:
 				console.log(`could not register ${route}`);
@@ -15,5 +18,15 @@ module.exports = (app, registry) => {
 	};
 }
 
-const createResponse = (responder) => 
-	(req, res) => res.send(responder(req));
+const createResponse = (responder) => {
+	if (responder && typeof responder === 'function')
+		return (req, res) => res.send(responder(req));
+
+	if (responder && typeof responder === 'object')
+		return (req, res) => res.send(responder);
+
+	if (responder && typeof responder === 'number' && isFinite(responder))
+		return (req, res) => res.status(responder) && res.send({});
+
+	return (req, res) => res.send({});
+}
