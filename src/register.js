984cc4bd56e98979dffff0779792890aad5a3fa6
Adam Cotner <acotner@tendrilinc.com>
2018-09-14 11:42:27 -0600

    rescope validation

--- src/register.js
+++ src/register.js
@@ -1,31 +1,31 @@
 module.exports = (app, registry) => {
 	for (let { route, method, response } of registry) {
 		
-		if (!app[method] || typeof app[method] !== 'function')
-			throw new Error(`could not register ${route}: "${method}"" is not a valid method`)
-
-		app[method](route, createResponder(route, response))
+		createResponder(app, route, method, response)
 		console.log(`registered ${method} for endpoint ${route}`);
 	}
 }
 
-const createResponder = (route, response) => {
+const createResponder = (app, route, method, response) => {
+	if (!app[method] || typeof app[method] !== 'function')
+		throw new Error(`could not register ${route}: "${method}"" is not a valid method`)
+
 	if (typeof response === 'function')
-		return (req, res) => res.send(response(req))
+		return app[method](route, (req, res) => res.send(response(req)))
 
 	if (typeof response === 'number' && isFinite(response))
-		return (req, res) => res.status(response) && res.send({})
+		return app[method](route, (req, res) => res.status(response) && res.send({}))
 
-	if (response && typeof response === 'object')
-		return (req, res) => {
-			if (response.headers)
-				res.set(response.headers)
+	if (response && typeof response === 'object') {
+		if (response.headers && typeof response.headers !== 'object')
+			throw new Error(`could not register ${route}: "headers" is not of an appropriate type`)
 
-			if (response.status)
-				res.status(response.status)
+		if (typeof response.body === 'undefined')
+			throw new Error(`could not register ${route}: "body" is not defined`)
 
-			res.send(response.body)
-		}
+		return app[method](route, (req, res) => 
+			res.set(response.headers || {}) && res.status(response.status || 200) && res.send(response.body))
+	}
 
 	throw new Error(`could not register ${route}: "response" is not of an appropriate type`)
 }
