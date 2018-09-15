14cbe3c03d5c46dde9f0542b1b8eb8d3b4e54911
AdamCotner <adamstevencotner@gmail.com>
2018-09-15 12:35:03 -0600

    split out file configuration

--- src/args/parse_args.js
+++ src/args/parse_args.js
@@ -1,36 +1,9 @@
-const path = require('path')
-const fs = require('fs')
 const buildConfig = require('./build_config.js')
+const fileConfig = require('./file_config.js')
 
 module.exports = (args) => {
-  let port, abspath
-
   if (args.length === 0)
     return buildConfig()
 
-  for (let i = 0; i < args.length; i++) {
-    if (args[i].startsWith('--port=')) {
-      port = args[i].substring(7)
-    } else if (args[i] === '--port' || args[i] === '-p') {
-      port = args[++i]
-    } else {
-      abspath = path.join(process.cwd(), args[i])
-    }
-  }
-
-  if (!fs.existsSync(abspath)) {
-      throw new Error(`no file found at ${abspath}`)
-  }
-
-  if (path.extname(abspath) !== '.js') {
-      throw new Error(`file must be ".js". got "${path.extname(abspath)}" instead`)
-  }
-
-  try {
-     var config = require(abspath)
-  } catch (e) {
-    throw new Error(`could not import file at ${abspath}`)
-  }
-
-  return { config, port: port || 8000 }
+  return fileConfig(args)
 }
